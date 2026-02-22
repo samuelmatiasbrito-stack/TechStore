2ac7a41a03c835a88ff01cfabedd8bc243408813
Samuel Matias <samuelmatiasbrito@gmail.com>
2026-02-22 18:46:48 -0300

    __del__ implementado

--- TechSQL.py
+++ TechSQL.py
@@ -1,27 +1,14 @@
-import mysql.connector
-import json
-import pandas as pd
+
 from database import DataBaseModel
+databasemodel = DataBaseModel('tech_store')
+print(databasemodel.mostrar_database())
+print(databasemodel.mostrar_database())
 
-with open("userpassword.json", "r") as locked:
-    userpassword = json.load(locked)
-
-with mysql.connector.connect(
-    host="localhost",
-    user=userpassword['USER'],
-    password=userpassword['PASS'],
-    database="tech_store"
-) as sql_database:
-    tech_store = pd.read_sql("SELECT * FROM produtos", sql_database)
-
-databasemodel = DataBaseModel(tech_store)
-print(tech_store)
-while True:
-    print('DataBase carregada')
-    opcao = input('(1) Exportar para Excel \n (2) \n Adicionar novo produto \n (3) Conferir produto específico: ')
-    if opcao == '1':
-        databaseexcelname = input('Nome do Arquivo: ')
-        print('Exportando...')
-        databasemodel.export_excel(databaseexcelname)
-        pass
-    elif opcao == '2':
+#     print('DataBase carregada')
+#     opcao = input('(1) Exportar para Excel \n (2) Adicionar novo produto \n (3) Conferir produto específico: ')
+#     if opcao == '1':
+#         databaseexcelname = input('Nome do Arquivo: ')
+#         print('Exportando...')
+#         databasemodel.export_excel(databaseexcelname)
+#         pass
+#     elif opcao == '2':
--- database.py
+++ database.py
@@ -1,6 +1,34 @@
+import mysql.connector
+import json
+import pandas as pd
+with open("userpassword.json", "r") as locked:
+    userpassword = json.load(locked)
+
 class DataBaseModel:
+    
     def __init__(self, arquivo):
         self.database = arquivo
+        self.connector = self.obter_connector()
+
     def export_excel(self, nome):
-        self.database.to_excel(f'{nome}.xlsx', sheet_name='DataBase', header=False)
-        
+        return self.database.to_excel(f'{nome}.xlsx', sheet_name='DataBase', header=False)
+    
+    def obter_connector(self):
+            with open('userpassword.json', 'r') as locked:
+                userpassword = json.load(locked)
+                return mysql.connector.connect(
+                    host = 'localhost',
+                    user = userpassword['USER'],
+                    password = userpassword['PASS'],
+                    database = self.database
+                )
+            
+    def add_produto(self,nome, preco, marca, categoria, especificacoes):
+         pass
+      
+    def mostrar_database(self):
+         return pd.read_sql('SELECT * from produtos', self.connector)
+    
+    def __del__(self):
+         self.connector.close()
+         print('Conexão encerrada')
